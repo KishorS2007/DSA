ada10a49ca5dfee27e176599395f9a94863b23fc
KISHOR S <kishorsuresh210@gmail.com>
2026-09-16 21:31:58 +0530

    Time: 8 ms (67.76%) | Memory: 46.3 MB (38.33%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] freq = new int[26];
+        int maxFreq = 0 , l = 0 , maxLen = 0;
+
+        for(int r = 0 ; r < s.length() ; r++){
+            char c = s.charAt(r);
+            maxFreq = Math.max(maxFreq,++freq[c-'A']);
+
+            while(r - l + 1 - maxFreq > k){
+                freq[s.charAt(l)-'A']--;
+                maxFreq = 0;
+                for(int i : freq) maxFreq = Math.max(maxFreq,i);
+                l++;
+            }
+
+            maxLen = Math.max(maxLen , r-l+1);
+        }
+
+        return maxLen;
+    }
+}
