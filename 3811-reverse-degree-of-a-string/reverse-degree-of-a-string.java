3832e8feafd1bc8b175d3de89b48242365df8ff4
KISHOR S <kishorsuresh210@gmail.com>
2026-09-20 10:22:46 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (18.31%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int total = 0;
+        for(int i = 0 ; i < s.length() ; i++){
+            total += (26 - (s.charAt(i)-'a')) * (i+1);
+        }
+
+        return total;
+    }
+}
