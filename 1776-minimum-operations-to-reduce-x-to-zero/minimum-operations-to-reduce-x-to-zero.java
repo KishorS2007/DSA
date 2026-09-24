b603007a1103caeaff4626cca4f7b3f41caa1341
KISHOR S <kishorsuresh210@gmail.com>
2026-09-24 16:28:33 +0530

    Time: 5 ms (35.84%) | Memory: 102.1 MB (44.91%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.java
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.java
@@ -1,38 +1,20 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
-        final int n = nums.length;
-        
-        long[] prefix = new long[n+1];
-        long[] suffix = new long[n+1];
+        long target = -x;
+        for(int i : nums) target += i;
 
-        for(int i = 0 ; i  < n ; i++){
-            prefix[i+1] = prefix[i] + nums[i];
+        if(target < 0) return -1;
+        if(target == 0) return nums.length;
+
+        int l = 0 , maxLen = 0, sum = 0;
+
+        for(int r = 0 ; r < nums.length ; r++){
+            sum += nums[r];
+            while(sum > target) sum -= nums[l++];
+
+            if(sum == target) maxLen = Math.max(maxLen , r - l + 1); 
         }
 
-        for(int i = 1 ; i < n ; i++){
-            suffix[i] = suffix[i-1] + nums[n-i];
-        }
-
-        int ans = n + 1; // impossible
-        for(int i = 0 ; i <= n ; i++){
-            if(prefix[i] > x) break;
-
-            int nextIdx = find(suffix , x - prefix[i]);
-            ans = Math.min(ans , nextIdx + i);
-        }
-
-        return ans > n ? -1 : ans;
-    }
-
-    private static int find(long[] arr , long target){
-        int l = 0 , r = arr.length - 1;
-        while(l <= r){
-            int mid = l + (r - l) / 2;
-            if(arr[mid] == target) return mid;
-            else if(arr[mid] >= target) r = mid-1;
-            else l = mid+1;
-        }
-
-        return arr.length + 1;
+        return maxLen != 0 ? nums.length - maxLen : -1;
     }
 }
